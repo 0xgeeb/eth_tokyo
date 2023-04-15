2c2849abd67ad91ad6f53e9d950869123a281f66
0xgeeb <0xgeeb@gmail.com>
2023-04-15 13:15:53 +0900

    setup

--- foundry/src/Counter.sol
+++ foundry/src/Counter.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import { ERC721 } from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+
+
 contract Counter {
     uint256 public number;
 
